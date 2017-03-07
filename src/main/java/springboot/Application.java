9cc7c36a258c29a81119429e2b1cd462a9cda322
bhabani.das@gmail.com <bhabani.das@gmail.com>
2017-03-06 22:17:28 -0500

    initial setup
    in spring boot

---
+++ src/main/java/springboot/Application.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author bhabanidas
+ *
+ */
+@SpringBootApplication
+public class Application {
+
+	/**
+	 * @param args
+	 */
+	
+	public static void main(String[] args) {
+	        SpringApplication.run(Application.class, args);
+	    }
+
+
+}
